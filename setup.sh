3e78ad5b5719e3488457939a0994b1707af2cd22
Jimmy Sambuo <jsambuo+github@gmail.com>
2024-06-08 13:17:21 -0400

    feat: add Git configuration and global .gitignore setup
    - Add .gitconfig file with user information, difftool, mergetool, commit template, and git-lfs settings
    - Add .gitignore_global file with global ignore rules
    - Update setup.sh to create symbolic links for .gitconfig and .gitignore_global

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # Create symbolic links
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # Source the .zshrc to apply changes immediately
 source ~/.zshrc
